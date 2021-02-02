7fe9b2ebf3b567d207e8bdb93b395e1b3e4ba877
Elias Calado <eliascalado@hotmail.com>
2021-02-02 11:08:06 -0300

    projeto criado

---
+++ src/main/java/br/com/mpsuporteti/cafenaestrada/CafenaestradaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mpsuporteti.cafenaestrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafenaestradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafenaestradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mpsuporteti/cafenaestrada/CafenaestradaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpsuporteti.cafenaestrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafenaestradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
